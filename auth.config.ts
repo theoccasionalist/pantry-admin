3423e8da5541d80a71e90547711bb7f9930c79cc
theoccasionalist <mrmccall07@gmail.com>
2020-03-22 12:28:35 -0400

    the document says so

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,6 @@
 export default {
     domain: 'young-cell-9742.auth0.com',
     client_id: 'baw80wc6kObTX8PwZQTfcqaExteejyaS',
+    audience: 'volunteer',
     redirect_uri: `${window.location.origin}/callback`
 };
